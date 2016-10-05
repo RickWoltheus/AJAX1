6eda0efbe9d88049ddefa1079ffbaea1387ded65
RickWoltheus <rwoltheus@gmail.com>
2016-10-05 14:06:21 +0200

    start
    start

---
+++ PHP/select.php
@@ -0,0 +1,14 @@
+<?php
+
+while($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['FirstName'] . "</td>";
+    echo "<td>" . $row['LastName'] . "</td>";
+    echo "<td>" . $row['Age'] . "</td>";
+    echo "<td>" . $row['Hometown'] . "</td>";
+    echo "<td>" . $row['Job'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+mysqli_close($con);
+?>
---
+++ PHP/show_table.php
@@ -0,0 +1,9 @@
+<?php 
+echo "<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+<th>Age</th>
+<th>Hometown</th>
+<th>Job</th>
+</tr>";
